68a98831e597facfc83aa71fd823373085094225
Edward Korkollie <korkollieed@gmail.com>
2019-06-10 11:06:06 -0400

    Another update

--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -1,29 +1,40 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class ListUtility {
-    public Boolean add(int i) {
-        return null;
+    List<Integer> list = new ArrayList<>();
+
+    public Boolean add(Integer i) {
+
+        return list.add(i);
     }
 
     public Integer size() {
-        return null;
+
+        return list.size();
     }
 
     public List<Integer> getUnique() {
-        return null;
+
+        return new ArrayList<>(new HashSet<>(list));
     }
 
     public String join() {
-        return null;
+        String jon = list.toString();
+        return jon.substring(1, jon.length()-1);
     }
 
     public Integer mostCommon() {
+        Integer[] transformed = new Integer[list.size()];
+        ArrayList arrUtil = new ArrayList();
         return null;
     }
 
     public Boolean contains(Integer valueToAdd) {
-        return null;
+
+        return list.contains(valueToAdd);
     }
 }
